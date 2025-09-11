a1930c76422e6d9ac165136185f18f1370e89372
pedrofortesgonzalez <pedrofortesgonzalez94@gmail.com>
2025-09-11 13:49:05 +0200

    Add files via upload

--- Anexo_1_construir_queries/fase_1_udfs.py
+++ Anexo_1_construir_queries/fase_1_udfs.py
@@ -7,18 +7,8 @@ Incluye funciones para limpiar encabezados, convertir formato de términos y tra
 entre sintaxis de diferentes bases de datos.
 """
 
-import re, pandas as pd
-from typing import Optional
-
-def viz():
-    """
-    Amplía las visualizaciones por defecto de pandas para mejorar la consulta de DataFrames directamente en el notebook, sin resultados acortados.
-    """
-    pd.set_option('display.max_rows', None)       # show all rows
-    pd.set_option('display.max_columns', None)    # show all cols
-    pd.set_option('display.width', None)          # not restricting width
-    pd.set_option('display.max_colwidth', None)   # show all content in every cell
-    
+import re
+   
 
 def rm_head(string: str) -> str:
     """
@@ -38,44 +28,3 @@ def rm_head(string: str) -> str:
     header = r'^#.*$'
     string = re.sub(pattern=header, repl='', string=string, flags=re.MULTILINE)
     return string.strip()
-
-def convertir(query: str) -> str:
-    """
-    Convierte términos entrecomillados a formato con llaves, excepto si terminan en asterisco.
-    
-    Args:
-        query (str): Consulta con términos entrecomillados
-        
-    Returns:
-        str: Consulta con términos convertidos a formato de llaves
-        
-    Example:
-        >>> query = 'TITLE-ABS-KEY("biomarker" OR "protein*")'
-        >>> convertir(query)
-        'TITLE-ABS-KEY({biomarker} OR "protein*")'
-    """
-    def rep_comillas(match: re.Match) -> str:
-        """Función auxiliar para procesar cada coincidencia."""
-        term = match.group(1)
-        return f'"{term}"' if term.endswith('*') else f'{{{term}}}'
-    
-    return re.sub(r'"([^"]+)"', rep_comillas, query)
-
-def sp2wos(scopus_query: str) -> str:
-    """
-    Traduce una consulta de formato Scopus a formato Web of Science.
-    
-    Args:
-        scopus_query (str): Consulta en formato Scopus
-        
-    Returns:
-        str: Consulta traducida a formato Web of Science
-        
-    Example:
-        >>> sp2wos('TITLE-ABS-KEY({biomarker})')
-        'TS="biomarker"'
-    """
-    wos_query = scopus_query.replace('{', '"')
-    wos_query = wos_query.replace('}', '"')
-    wos_query = wos_query.replace('TITLE-ABS-KEY', 'TS=')
-    return wos_query
